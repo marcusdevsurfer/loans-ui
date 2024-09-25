db27f8c1282f111b986eabdd4ccdca8564bde111
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-24 20:23:43 -0600

    refact: uid changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,13 @@ function App() {
   const payByMonth = loansState.filter((loan) => loan.interest > 0).map((loan) => (loan.amount * loan.interest) / 100).reduce((pv, cv) => pv + cv)
   return (
     <Container>
-      <h1 className='my-4 dashboard-title'>Panel de Administracion de Prestamos</h1>
-      <Row className="justify-content-center">
+      <Row className="justify-content-center my-4">
+        <h1 className=' dashboard-title'>Panel de Administracion de Prestamos</h1>
         <DashboardCard text={"Prestamos Activos"} data={loansState.length} icon={<CiDollar size={'20'} />} />
         <DashboardCard text={"Total Prestado"} data={total} icon={<CiAlignBottom size={'20'} />} dollarSign />
         <DashboardCard text={"Clientes"} data={loansState.length} icon={<CiUser size={'20'} />} />
         <DashboardCard text={"Pagos del mes"} data={payByMonth} icon={<CiCalendarDate size={'20'} />} dollarSign />
       </Row>
-      <Stack direction='horizontal' className='my-4' gap={1}>
-        <h2 className='dashboard-title'>Lista de prestamos</h2>
-        <Button variant='dark ms-auto'>Nuevo Préstamo</Button>
-      </Stack>
       <Row>
         <LoansTable loans={loansState} />
       </Row>
