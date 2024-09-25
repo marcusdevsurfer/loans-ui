491f74ec685a454876df179b480b046d71ed4066
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-24 20:05:17 -0600

    refact: ui changes

--- src/components/LoansTable.jsx
+++ src/components/LoansTable.jsx
@@ -1,27 +1,25 @@
-import './css/LoansTable.css'
-
 import Table from 'react-bootstrap/Table'
-
+import './css/LoansTable.css'
 
 export const LoansTable = ({ loans }) => {
     return (
-        <Table>
+        <Table striped className='p-3 border border-1'>
             <thead>
                 <tr>
-                    <th className='text-red'>Cliente</th>
-                    <th>Monto</th>
-                    <th>Interes</th>
-                    <th>Estado</th>
+                    <th className='text-secondary p-3'>Cliente</th>
+                    <th className='text-secondary p-3'>Monto</th>
+                    <th className='text-secondary p-3'>Interes</th>
+                    <th className='text-secondary p-3'>Estado</th>
                 </tr>
             </thead>
             <tbody>
                 {
                     loans.map((loan) =>
                         <tr key={loan?.id}>
-                            <td>{`${loan?.client}`}</td>
-                            <td>{`$${loan?.amount.toLocaleString('en')}`}</td>
-                            <td>{`${loan?.interest}%`}</td>
-                            <td>{loan?.status === 'active' && 'Activo'}</td>
+                            <td className='p-3'>{`${loan?.client}`}</td>
+                            <td className='p-3'>{`$${loan?.amount.toLocaleString('en')}`}</td>
+                            <td className='p-3'>{`${loan?.interest}%`}</td>
+                            <td className='p-3'>{loan?.status === 'active' && 'Activo'}</td>
                         </tr>
                     )
                 }
