a41135f240d7bb58cbc9aa00eacea74742a84659
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-27 16:53:58 -0600

    refact: router implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-//import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route } from 'wouter';
+import { LoanDetails } from './components/LoanDetails.jsx';
+import App from './App.jsx'
+
+const Router = () => (
+  <div>
+    <Route path="/" component={App}></Route>
+    <Route path="/loan-details/:id">{params => <LoanDetails id={params.id}/>}</Route>
+  </div>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router />
 )
