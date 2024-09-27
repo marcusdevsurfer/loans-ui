2b8a3410c0487366c8bee0fe22dbc17db8c0a694
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-27 17:10:22 -0600

    add: loan details validation

--- src/components/LoanDetails.jsx
+++ src/components/LoanDetails.jsx
@@ -1,5 +1,28 @@
-export const LoanDetails = () => {
-  return (
-    <div>LoanDetails</div>
-  )
+import { useEffect, useState } from "react"
+import { getLoanById } from "../service/LoanService"
+
+export const LoanDetails = ({ id }) => {
+    const [loanState, setLoansState] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setLoansState(getLoanById(id))
+        setIsLoading(false)
+    }, [])
+
+    return (
+        isLoading
+            ?
+            'Loading'
+            :
+            !loanState
+                ?
+                <h1>
+                    No existe
+                </h1>
+                :
+                <h1>
+                    {loanState?.client}
+                </h1>
+    )
 }
