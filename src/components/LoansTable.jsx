a943f8d6c6a44849296efe90a91c1a1ddc13781d
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-27 18:31:52 -0600

    add: link to watch loan details

--- src/components/LoansTable.jsx
+++ src/components/LoansTable.jsx
@@ -1,14 +1,13 @@
 import Table from 'react-bootstrap/Table'
 import Stack from 'react-bootstrap/Stack'
-import Button from 'react-bootstrap/Button'
+import { Link } from 'wouter'
 import './css/LoansTable.css'
 
 export const LoansTable = ({ loans }) => {
     return (
         <div className='loans-table-section'>
-            <Stack direction='horizontal' className='my-3'>
-                <h2 className='dashboard-title'>Lista de prestamos</h2>
-                <Button variant='dark' className='ms-auto'>Nuevo Préstamo</Button>
+            <Stack direction='horizontal'>
+                <h2 className='dashboard-title mb-3'>Lista de prestamos</h2>
             </Stack>
             <Table responsive striped borderless>
                 <thead>
@@ -17,6 +16,7 @@ export const LoansTable = ({ loans }) => {
                         <th className='text-secondary'>Monto</th>
                         <th className='text-secondary'>Interes</th>
                         <th className='text-secondary'>Estado</th>
+                        <th className=''></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -27,6 +27,11 @@ export const LoansTable = ({ loans }) => {
                                 <td className='p-3'>{`$${loan?.amount.toLocaleString('en')}`}</td>
                                 <td className='p-3'>{`${loan?.interest}%`}</td>
                                 <td className='p-3'>{loan?.status === 'active' && 'Activo'}</td>
+                                <td>
+                                    <Link href={`/loan-details/${loan?.id}`}>
+                                        <a className='btn btn-sm btn-dark'>Ver</a>
+                                    </Link>
+                                </td>
                             </tr>
                         )
                     }
