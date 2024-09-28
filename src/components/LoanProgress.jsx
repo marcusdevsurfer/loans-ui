6606643e736375f1497ff309d1ce2af0875ea23f
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-27 23:37:45 -0600

    refact: loan progress component with static content

--- src/components/LoanProgress.jsx
+++ src/components/LoanProgress.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import ProgressBar from "react-bootstrap/ProgressBar"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import '../App.css'
+import './css/LoanDetails.css'
 
 export const LoanProgress = () => {
-  return (
-    <div>LoanProgress</div>
-  )
+    return (
+        <div className="loan-details-card">
+            <h1 className="font-title mb-4">Progreso del prestamo</h1>
+            <ProgressBar variant="dark" now={50} />
+            <Row>
+                <Col>
+                    <p className="text-start font-subtitle p-1">Total pagado: $1000</p>
+                </Col>
+                <Col>
+                    <p className="text-end font-subtitle p-1">Restante: $2000</p>
+                </Col>
+            </Row>
+        </div>
+    )
 }
