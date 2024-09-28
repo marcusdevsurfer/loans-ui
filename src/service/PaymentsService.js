f9f6c038665e78a0b243c2b7a9b7f75b6a61c72a
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-28 01:15:15 -0600

    add: payments service

---
+++ src/service/PaymentsService.js
@@ -0,0 +1,16 @@
+export const payments = [
+    {
+        "date": new Date(2024, 8, 16),
+        "loanId": 7,
+        "amount": 3000
+    },
+    {
+        "date": new Date(2024, 8, 23),
+        "loanId": 7,
+        "amount": 3000
+    }
+]
+
+export const getPaymentsByLoanId = (id) => {
+    return payments.filter((payment) => payment.loanId == id)
+}
