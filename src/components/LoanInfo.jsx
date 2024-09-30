92daa2bdbcae1dc04ddd627f7195662f38e57f91
marcusdevsurfer <marcus.sea13@gmail.com>
2024-09-29 21:23:46 -0600

    code clean in component

--- src/components/LoanInfo.jsx
+++ src/components/LoanInfo.jsx
@@ -4,8 +4,10 @@ import '../App.css'
 import './css/LoanDetails.css'
 
 export const LoanInfo = ({ loan }) => {
-    const interestAmount = (loan.amount * loan.interest) / 100
-    const installmentAmount = loan.amount / loan.installments
+    const { amount, interest, installments } = loan
+    const interestTotal = (amount * interest) / 100
+    const loanTotalAmount = (interestTotal + amount)
+    const installmentAmount = (amount / installments)
     return (
         <div className='loan-details-card'>
             <h1 className='font-title mb-4'>Detalles de Prestamo</h1>
@@ -16,27 +18,32 @@ export const LoanInfo = ({ loan }) => {
                 </Col>
                 <Col>
                     <h5 className='font-subtitle'>Monto de prestamo</h5>
-                    <p className='font-text'>${loan?.amount.toLocaleString()}</p>
+                    <p className='font-text'>${amount.toLocaleString()}</p>
+                </Col>
+                <Col>
+                    <h5 className='font-subtitle'>Pago total</h5>
+                    <p className='font-text'>${loanTotalAmount.toLocaleString()}</p>
                 </Col>
             </Row>
             <Row direction='horizontal'>
                 {
-                    loan.installments &&
+                    installments &&
                     <Col>
                         <h5 className='font-subtitle'>Cuotas</h5>
-                        <p className='font-text'>{loan?.installments}</p>
+                        <p className='font-text'>{installments}</p>
                     </Col>
                 }
 
                 {
-                    loan.interest > 0 &&
+                    interest > 0 &&
                     <Col>
                         <h5 className='font-subtitle'>Monto de cuota</h5>
-                        {!loan.installments && <p className='font-text'>{`$${interestAmount.toLocaleString()}`}</p>}
+                        {!loan.installments && <p className='font-text'>{`$${interestTotal.toLocaleString()}`}</p>}
                         {loan.installments && <p className='font-text'>{`$${installmentAmount.toLocaleString()}`}</p>}
                     </Col>
                 }
-
+                <Col>
+                </Col>
             </Row>
         </div>
     )
