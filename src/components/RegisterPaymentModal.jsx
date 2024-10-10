e812a4a5198b975219800b2e68f217e8ea078d98
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-10 00:33:49 -0600

    add: handling added

--- src/components/RegisterPaymentModal.jsx
+++ src/components/RegisterPaymentModal.jsx
@@ -4,32 +4,34 @@ import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import { useParams } from 'wouter';
 
-export const RegisterPaymentModal = (props) => {
+export const RegisterPaymentModal = ({ show, setModalShow, fetchData }) => {
     const { id } = useParams()
     const [dateState, setDateState] = useState("")
     const [amountState, setAmountState] = useState("")
-    const [loanIdState, setLoanIdState] = useState("")
 
     const savePayment = async () => {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-
         const data = {
             "date": dateState,
             "amount": amountState,
             "loanId": parseInt(id)
         }
-
         const response = await fetch("http://127.0.0.1:3000/api/payments", {
             method: "POST",
             body: JSON.stringify(data),
             headers: myHeaders,
         })
     }
+    const handleForm = () => {
+        savePayment()
+        setModalShow(false)
+        fetchData(id)
+    }
 
     return (
         <Modal
-            {...props}
+            show={show}
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered
@@ -40,17 +42,17 @@ export const RegisterPaymentModal = (props) => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Form>
+                <Form onSubmit={handleForm}>
                     <Form.Group>
                         <Form.Label>Fecha</Form.Label>
-                        <Form.Control type='date' onChange={(evn) => setDateState(evn.target.value)}></Form.Control>
+                        <Form.Control required type='date' onChange={(evn) => setDateState(evn.target.value)}></Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Cantidad</Form.Label>
-                        <Form.Control type='number' onChange={(evn) => setAmountState(evn.target.value)}></Form.Control>
+                        <Form.Control required type='number' onChange={(evn) => setAmountState(evn.target.value)}></Form.Control>
                     </Form.Group>
                     <Modal.Footer>
-                        <Button size='sm' variant='dark' onClick={savePayment}>Guardar</Button>
+                        <Button type='submit' size='sm' variant='dark'>Guardar</Button>
                     </Modal.Footer>
                 </Form>
             </Modal.Body>
