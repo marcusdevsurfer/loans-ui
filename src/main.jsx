71d69d8e54dfcdc5bdd8ab1e1e72ed8874d136b1
marcusdevsurfer <marcus.sea13@gmail.com>
2024-10-12 16:03:43 -0600

    change of routes name

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx'
 import { Route } from 'wouter';
 import { LoanView } from './components/LoanView.jsx';
-import App from './App.jsx'
+import { LoanCustomerView } from './components/LoanCustomerView.jsx';
 
 const Router = () => (
   <div>
     <Route path="/" component={App}></Route>
-    <Route path="admin/loan-details/:id">{params => <LoanView id={params.id}/>}</Route>
+    <Route path="admin/loan-details/:id">{params => <LoanView id={params.id} />}</Route>
+    <Route path="customer/loan-details/:id">{params => <LoanCustomerView id={params.id} />}</Route>
   </div>
 )
 
