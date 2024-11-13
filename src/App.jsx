ff94f698b7feb7d0468e140711c2640069cd3907
marcusdevsurfer <marcus.sea13@gmail.com>
2024-11-13 02:17:30 -0600

    refact: code clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { getloans } from './service/LoanService';
+import { useEffect, useState } from 'react';
+import { fetchAndSetLoans } from './service/LoanService';
 import { DashboardCard } from './components/DashboardCard'
 import { LoansTable } from './components/LoansTable';
 import Container from 'react-bootstrap/Container'
@@ -9,21 +9,43 @@ import './App.css'
 
 
 function App() {
-  const [loansState, setLoansState] = useState(getloans)
-  const total = loansState.map((e) => e.amount).reduce((pv, cv) => pv + cv)
-  const payByMonth = loansState.filter((loan) => loan.interest > 0).map((loan) => (loan.amount * loan.interest) / 100).reduce((pv, cv) => pv + cv)
+  const [loansState, setLoansState] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetchAndSetLoans(setLoansState)
+    setIsLoading(false)
+  }, [])
+
+  // const payByMonth = loansState.filter((loan) => loan?.interest > 0).map((loan) => (loan?.amount * loan?.interest) / 100).reduce((pv, cv) => pv + cv)
+
+  const sumValuesByKey = (collection, key) => {
+    return collection.map((e) => e[key]).reduce((pv, cv) => pv + cv, 0);
+  }
+
   return (
     <Container>
-      <Row className="justify-content-center my-4">
-        <h1 className=' dashboard-title'>Panel de Administracion de Prestamos</h1>
-        <DashboardCard text={"Prestamos Activos"} data={loansState.length} icon={<CiDollar size={'20'} />} />
-        <DashboardCard text={"Total Prestado"} data={total} icon={<CiAlignBottom size={'20'} />} dollarSign />
-        <DashboardCard text={"Clientes"} data={loansState.length} icon={<CiUser size={'20'} />} />
-        <DashboardCard text={"Pagos del mes"} data={payByMonth} icon={<CiCalendarDate size={'20'} />} dollarSign />
+      <Row className="justify-content-center align-items-center my-4">
+        <h1 className='dashboard-title'>Panel de Administracion de Prestamos</h1>
+        <DashboardCard text={"Prestamos Activos"} data={loansState?.length} icon={<CiDollar size={'20'} />} />
+        <DashboardCard text={"Clientes"} data={loansState?.length} icon={<CiUser size={'20'} />} />
+        {
+          !isLoading &&
+          <DashboardCard text={"Total Prestado"} data={sumValuesByKey(loansState, 'amount')} icon={<CiAlignBottom size={'20'} />} dollarSign />
+        }
+        {/* <DashboardCard text={"Pagos del mes"} data={payByMonth} icon={<CiCalendarDate size={'20'} />} dollarSign /> */}
       </Row>
-      <Row>
-        <LoansTable loans={loansState} />
-      </Row>
+      {
+        isLoading && <h1>Cargando...</h1>
+      }
+      {
+        !isLoading &&
+        <Row>
+          <LoansTable />
+        </Row>
+      }
+
+
     </Container>
 
   )
