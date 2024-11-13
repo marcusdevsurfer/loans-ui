55d76e9d157d8e040263a08465c400954e5b26fe
marcusdevsurfer <marcus.sea13@gmail.com>
2024-11-13 02:44:05 -0600

    refact: first commit

--- src/components/LoanProgress.jsx
+++ src/components/LoanProgress.jsx
@@ -2,32 +2,57 @@ import { useEffect, useState } from "react"
 import ProgressBar from "react-bootstrap/ProgressBar"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-import { getPaymentsByLoanId } from "../service/PaymentsService"
+import Spinner from "react-bootstrap/Spinner"
+import Container from "react-bootstrap/Container"
+import { fetchLoanById } from "../service/LoanService"
 import '../App.css'
 import './css/LoanDetails.css'
 
-export const LoanProgress = ({ loan }) => {
-    const { id, amount, interest } = loan
-    const interestTotal = (amount * interest) / 100
-    const loanTotalAmount = (interestTotal + amount)
-    const [paymentsState, setPaymentsState] = useState([])
-    const progressPorcent = (paymentsState.length * 100) / loan.installments
-    const paidAmount = paymentsState.map((payment => payment.amount)).reduce((pv, cv) => pv + cv, 0)
+export const LoanProgress = ({ loanId }) => {
+    const [loanState, setLoanState] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [progressPorcent, setProgressPorcent] = useState(30)
+
     useEffect(() => {
-        setPaymentsState(getPaymentsByLoanId(id))
-    }, [])
+        fetchLoanById(loanId)
+            .then((data) => setLoanState(data))
+            .catch((error) => console.log(error))
+            .finally(() => setIsLoading(false))
+    }, [loanId])
+
+    const calculateInterest = () => {
+        if (loanState) {
+            const interest = loanState.amount * (loanState.interestRate / 100)
+            return interest
+        }
+    }
+
+    const calculateTotal = () => {
+        if (loanState) {
+            const total = loanState.amount + calculateInterest()
+            return total
+        }
+    }
     return (
-        <div className="loan-details-card">
+        <Container className="loan-details-card">
             <h1 className="font-title mb-4">Progreso del prestamo</h1>
-            <ProgressBar variant="dark" now={progressPorcent} label={`${progressPorcent}%`} />
-            <Row>
-                <Col>
-                    <p className="text-start font-subtitle p-1">Total pagado: ${paidAmount.toLocaleString()}</p>
-                </Col>
-                <Col>
-                    <p className="text-end font-subtitle p-1">Restante: -${(loanTotalAmount - paidAmount).toLocaleString()}</p>
-                </Col>
-            </Row>
-        </div>
+            {
+                isLoading
+                    ?
+                    <Spinner className='ms-auto' variant='dark' />
+                    :
+                    <>
+                        <ProgressBar variant="dark" now={progressPorcent} />
+                        <Row>
+                            <Col>
+                                <p className="text-start font-subtitle p-1">Total pagado: ${calculateInterest()}</p>
+                            </Col>
+                            <Col>
+                                <p className="text-end font-subtitle p-1">Restante: -${calculateTotal() - calculateInterest()}</p>
+                            </Col>
+                        </Row>
+                    </>
+            }
+        </Container>
     )
 }
