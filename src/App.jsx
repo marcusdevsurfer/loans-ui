34f7d90bd904f767111d8694a96554e0a17543a3
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-01 00:32:07 -0600

    app refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { DashboardCard } from './components/DashboardCard'
 import { LoansTable } from './components/LoansTable';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import { CiDollar, CiUser, CiAlignBottom, CiCalendarDate } from "react-icons/ci";
+import Spinner from 'react-bootstrap/Spinner'
+import { CiDollar, CiUser, CiAlignBottom } from "react-icons/ci";
 import './App.css'
-
 
 function App() {
   const [loansState, setLoansState] = useState([])
@@ -17,35 +17,30 @@ function App() {
     setIsLoading(false)
   }, [])
 
-  // const payByMonth = loansState.filter((loan) => loan?.interest > 0).map((loan) => (loan?.amount * loan?.interest) / 100).reduce((pv, cv) => pv + cv)
-
   const sumValuesByKey = (collection, key) => {
     return collection.map((e) => e[key]).reduce((pv, cv) => pv + cv, 0);
   }
 
   return (
     <Container>
-      <Row className="justify-content-center align-items-center my-4">
+      <Row className="justify-content-evenly align-items-center my-4">
         <h1 className='dashboard-title'>Panel de Administracion de Prestamos</h1>
-        <DashboardCard text={"Prestamos Activos"} data={loansState?.length} icon={<CiDollar size={'20'} />} />
-        <DashboardCard text={"Clientes"} data={loansState?.length} icon={<CiUser size={'20'} />} />
+        {
+          isLoading && <Spinner className='ms-auto' variant='dark' />
+        }
         {
           !isLoading &&
           <DashboardCard text={"Total Prestado"} data={sumValuesByKey(loansState, 'amount')} icon={<CiAlignBottom size={'20'} />} dollarSign />
         }
-        {/* <DashboardCard text={"Pagos del mes"} data={payByMonth} icon={<CiCalendarDate size={'20'} />} dollarSign /> */}
+        <DashboardCard text={"Prestamos Activos"} data={loansState?.length} icon={<CiDollar size={'20'} />} />
+        <DashboardCard text={"Clientes"} data={loansState?.length} icon={<CiUser size={'20'} />} />
       </Row>
-      {
-        isLoading && <h1>Cargando...</h1>
-      }
       {
         !isLoading &&
         <Row>
           <LoansTable />
         </Row>
       }
-
-
     </Container>
 
   )
