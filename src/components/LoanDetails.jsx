643856d18893be16e3e0a0699e503d90a0d3a2c2
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-01 00:42:01 -0600

    ui refactored

--- src/components/LoanDetails.jsx
+++ src/components/LoanDetails.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import { fetchLoanById } from '../service/LoanService'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -12,15 +13,17 @@ export const LoanDetails = ({ loanId }) => {
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        fetchLoanById(loanId)
-            .then((data) => {
-                setLoanState(data)
-                setIsLoading(false)
-            })
-            .catch(error => {
-                console.error(error)
-            })
+        const fetchData = async () => { 
+            const loan = await fetchLoanById(loanId)
+            setLoanState(loan)
+            setIsLoading(false)
+        }
+        fetchData()
     }, [loanId])
+
+    const calculateTotalPayment = (amount, interestRate) => {
+        return amount + (amount * (interestRate / 100))
+    }
 
     const formatNumber = (number) => {
         return number.toLocaleString('en-US', {
@@ -29,30 +32,38 @@ export const LoanDetails = ({ loanId }) => {
         });
     };
 
+    if (isLoading) {
+        return <Spinner className='ms-auto' variant='dark' />
+    }
+
     return (
-            <Container className='loan-details-card'>
-                <h1 className='font-title mb-4'>Detalles de Prestamo</h1>
-                {
-                    isLoading &&
-                    <Spinner className='ms-auto text-center' variant='dark' />
-                }
-                {
-                    loanState &&
-                    <Row className='align-items-center' >
-                        <Col>
-                            <h5 className='font-subtitle'>Cliente</h5>
-                            <p className='font-text'>{loanState?.borrower}</p>
-                        </Col>
-                        <Col>
-                            <h5 className='font-subtitle'>Monto de prestamo</h5>
-                            <p className='font-text'>${formatNumber(loanState?.amount)}</p>
-                        </Col>
-                        <Col>
-                            <h5 className='font-subtitle'>Interes</h5>
-                            <p className='font-text'>{loanState?.interestRate}%</p>
-                        </Col>
-                    </Row>
-                }
-            </Container>
+        <Container className='loan-details-card'>
+            <h1 className='font-title mb-4'>Detalles de Prestamo</h1>
+            {
+                loanState &&
+                <Row className='align-items-center' >
+                    <Col>
+                        <h5 className='font-subtitle'>Cliente</h5>
+                        <p className='font-text'>{loanState?.borrower}</p>
+                    </Col>
+                    <Col>
+                        <h5 className='font-subtitle'>Monto de prestamo</h5>
+                        <p className='font-text'>${formatNumber(loanState?.amount)}</p>
+                    </Col>
+                    <Col>
+                        <h5 className='font-subtitle'>Interes</h5>
+                        <p className='font-text'>{loanState?.interestRate}%</p>
+                    </Col>
+                    <Col>
+                        <h5 className='font-subtitle'>Pago total</h5>
+                        <p className='font-text'>${formatNumber(calculateTotalPayment(loanState?.amount, loanState?.interestRate))}</p>
+                    </Col>
+                </Row>
+            }
+        </Container>
     )
+}
+
+LoanDetails.propTypes = {
+    loanId: PropTypes.string.isRequired,
 }
