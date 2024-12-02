fb9f8c8c4365e0db178f162434aa5ad5493b0c99
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-01 22:22:08 -0600

    addition of api calls

--- src/service/PaymentsService.js
+++ src/service/PaymentsService.js
@@ -1,21 +1,12 @@
-export const payments = [
-    {
-        "date": new Date(2024, 8, 16),
-        "loanId": 7,
-        "amount": 3000
-    },
-    {
-        "date": new Date(2024, 8, 23),
-        "loanId": 7,
-        "amount": 3000
-    },
-    {
-        "date": new Date(2024, 8, 30),
-        "loanId": 7,
-        "amount": 3000
-    }
-]
+const API_URL_BASE = import.meta.env.VITE_API_URL;
+const API_URL_COMPLEMENT = '/api/payments'
+const API_URL = `${API_URL_BASE}${API_URL_COMPLEMENT}`
 
-export const getPaymentsByLoanId = (id) => {
-    return payments.filter((payment) => payment.loanId == id)
+export const fetchPayments = async () => {
+    const response = await fetch(API_URL)
+    return response.json()
+}
+export const fetchPaymentsByLoan = async (id) => {
+    const response = await fetch(`${API_URL}/${id}`)
+    return response.json()
 }
