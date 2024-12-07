8a95edcb2ab49938ec0be50ae487b74593df5dbc
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-07 14:52:38 -0600

    add createLoan function to handle loan creation API requests

--- src/service/LoanService.js
+++ src/service/LoanService.js
@@ -1,5 +1,7 @@
 const API_URL_BASE = import.meta.env.VITE_API_URL;
 const API_URL_COMPLEMENT = '/api/loans'
+const SAVE_LOAN_URL_COMPLEMENT = '/api/loans/save'
+const SAVE_LOAN_URL = `${API_URL_BASE}${SAVE_LOAN_URL_COMPLEMENT}`
 const API_URL = `${API_URL_BASE}${API_URL_COMPLEMENT}`
 
 export const fetchAndSetLoans = async (setLoans) => {
@@ -24,6 +26,21 @@ export const fetchLoans = async () => {
   } catch (error) {
     console.error('Error en la peticion')
     return error;
+  }
+}
+
+export const createLoan = async (loan) => {
+  try {
+    const response = await fetch(SAVE_LOAN_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loan)
+    })
+    return response;
+  } catch (error) {
+    console.error('Error en la peticion')
   }
 }
 
