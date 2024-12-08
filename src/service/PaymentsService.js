d26558b721ae4addd072d3ddebaeaf7ac616e85a
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-07 18:33:19 -0600

    add createPayment function to handle payment creation API requests

--- src/service/PaymentsService.js
+++ src/service/PaymentsService.js
@@ -1,17 +1,34 @@
 const API_URL_BASE = import.meta.env.VITE_API_URL;
 const API_URL_COMPLEMENT = '/api/payments'
+const SAVE_PAYMENT_URL_COMPLEMENT = '/api/payments/save'
+const SAVE_PAYMENT_URL = `${API_URL_BASE}${SAVE_PAYMENT_URL_COMPLEMENT}`
 const API_URL = `${API_URL_BASE}${API_URL_COMPLEMENT}`
 
 export const fetchPayments = async () => {
     const response = await fetch(API_URL)
     return response.json()
 }
+
 export const fetchPaymentsByLoan = async (id) => {
     try {
         const response = await fetch(`${API_URL}/${id}`)
         return response.json()
     }
     catch (error) {
+        console.error('Error:', error)
+    }
+}
+
+export const createPayment = async (payment) => {
+    const REQUEST_OPTIONS = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payment),
+    }
+    try {
+        const response = await fetch(SAVE_PAYMENT_URL, REQUEST_OPTIONS)
+        return response
+    } catch (error) {
         console.error('Error:', error)
     }
 }
