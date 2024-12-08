dc46fb5b37c723827bf79c4826cc1e9b204ee9fd
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-07 18:34:01 -0600

    refactor RegisterPaymentModal to use createPayment service and simplify form handling

--- src/components/RegisterPaymentModal.jsx
+++ src/components/RegisterPaymentModal.jsx
@@ -3,46 +3,36 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import { useParams } from 'wouter';
+import { createPayment } from '../service/PaymentsService';
 
 export const RegisterPaymentModal = ({ show, setModalShow, fetchData, onHide }) => {
     const { id } = useParams()
-    const API_URL_BASE = import.meta.env.VITE_API_URL;
-    const API_URL_COMPLEMENT = "/api/payments/save"
-    const API_URL = `${API_URL_BASE}${API_URL_COMPLEMENT}`
-
     const [dateState, setDateState] = useState("")
     const [amountState, setAmountState] = useState("")
 
-    const savePayment = async (date, amount, loanId) => {
-        let data = {
-            "date": date,
-            "amount": amount,
-            "loanId": loanId
-        }
-        const REQUEST_OPTIONS = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data),
+    const handleForm = async (e) => {
+        e.preventDefault()
+
+        const newPayment = {
+            date: dateState,
+            amount: amountState,
+            loan: id
         }
         try {
-            const response = await fetch(API_URL, REQUEST_OPTIONS)
-            if (response.ok) {
-                const data = await response.json()
-                console.log(data)
+            const response = await createPayment(newPayment)
+            if(response.status === 201) {
+                clearForm()
+                setModalShow(false)
+                fetchData()
             }
-        } catch {
-            (e) => {
-                console.log('There was a error.')
-                console.log(e)
-            }
+        } catch (error) {
+            console.error('Error:', error)
         }
     }
 
-    const handleForm = async (e) => {
-        e.preventDefault()
-        await savePayment(dateState, amountState, id)
-        await fetchData(id)
-        setModalShow(false)
+    const clearForm = () => {
+        setDateState("")
+        setAmountState("")
     }
 
     return (
