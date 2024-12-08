30b27e6b89577acd3685355dfc7f1ae1ff8f9c5d
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-07 19:56:16 -0600

    refactor: replace inline routing with Router component for improved structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx'
-import { Route } from 'wouter';
-import { LoanAdminView } from './components/LoanAdminView.jsx';
-import { LoanCustomerView } from './components/LoanCustomerView.jsx';
-
-const Router = () => (
-  <>
-    <Route path="/" component={App}></Route>
-    <Route path="admin/loan-details/:id">{params => <LoanAdminView id={params.id} />}</Route>
-    <Route path="customer/loan-details/:id">{params => <LoanCustomerView id={params.id} />}</Route>
-  </>
-)
+import { Router } from './components/Router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router />
