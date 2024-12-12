923904e56dbb735281ec953aa57c21519a54cb9e
marcusdevsurfer <marcus.sea13@gmail.com>
2024-12-12 16:40:45 -0600

    add: implement route for ApplyView in Router component

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,11 +3,13 @@ import { Route, Switch } from 'wouter';
 import { LoanAdminView } from './LoanAdminView.jsx';
 import { LoanCustomerView } from './LoanCustomerView.jsx';
 import { NotFound } from './NotFound.jsx';
+import { ApplyView } from './ApplyView.jsx';
 
 export const Router = () => {
     return (
         <Switch>
             <Route path="/" component={App}></Route>
+            <Route path="/apply" component={ApplyView}></Route>
             <Route path="admin/loan-details/:id">{params => <LoanAdminView id={params.id} />}</Route>
             <Route path="customer/loan-details/:id">{params => <LoanCustomerView id={params.id} />}</Route>
             <Route component={NotFound}></Route>
