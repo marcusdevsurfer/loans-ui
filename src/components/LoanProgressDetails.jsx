90146cf03793ad91cf5fee2747685cb5cb1c4eec
marcusdevsurfer <marcus.sea13@gmail.com>
2025-01-02 17:52:53 -0600

    add: implement LoanProgressDetails component for displaying loan payment progress

---
+++ src/components/LoanProgressDetails.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Spinner from 'react-bootstrap/Spinner'
+import '../App.css'
+
+import { fetchPaymentsByLoan } from '../service/PaymentsService'
+
+export const LoanProgressDetails = ({ loanId }) => {
+    const [payments, setPayments] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetchData(loanId)
+    }, [loanId])
+
+    const fetchData = async (id) => {
+        try {
+            const response = await fetchPaymentsByLoan(id)
+            const data = await response.json()
+            setPayments(data)
+            setIsLoading(false)
+        } catch {
+            (e) => console.log(e)
+        }
+    }
+
+    return (
+        <Container className='card'>
+            <h1 className='font-title mb-2'>Progreso</h1>
+            <Row>
+                {
+                    isLoading
+                        ?
+                        <Col  className='text-center'>
+                            <Spinner variant='dark' animation="border" role="status" />
+                            <p className='text-muted'>Cargando detalles, por favor espera.</p>
+                        </Col>
+                        :
+                        payments.length > 0
+                        ?
+                        <>
+                            <Col>
+                                {`Número de pagos realizados: ${payments.length}`}
+                            </Col>
+                            <Col>
+                                Monto total pagado
+                            </Col>
+                        </>
+                        :
+                        <Col className='text-center'>
+                            No hay pagos realizados
+                        </Col>
+                }
+            </Row>
+        </Container >
+    )
+}
