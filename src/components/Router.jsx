1f4b581bbfc39dec3f197dfaa2a06f2fecccbf47
marcusdevsurfer <marcus.sea13@gmail.com>
2025-01-10 14:20:15 -0600

    feat: implement redirect from root to dashboard in Router component

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,5 @@
 import App from '../App.jsx';
-import { Route, Switch } from 'wouter';
+import { Route, Switch, Redirect } from 'wouter';
 import { LoanAdminView } from './LoanAdminView.jsx';
 import { LoanCustomerView } from './LoanCustomerView.jsx';
 import { NotFound } from './NotFound.jsx';
@@ -7,11 +7,12 @@ import { ApplyView } from './ApplyView.jsx';
 
 export const Router = () => {
     return (
-        <Switch>
-            <Route path="/" component={App}></Route>
+        <Switch >
+            <Route path="/" component={() => <Redirect to='/dashboard'/>}></Route>
+            <Route path="/dashboard" component={App}></Route>
             <Route path="/apply" component={ApplyView}></Route>
-            <Route path="admin/loan-details/:id">{params => <LoanAdminView id={params.id} />}</Route>
-            <Route path="customer/loan-details/:id">{params => <LoanCustomerView id={params.id} />}</Route>
+            <Route path="/admin/loan-details/:id">{params => <LoanAdminView id={params.id} />}</Route>
+            <Route path="/customer/loan-details/:id">{params => <LoanCustomerView id={params.id} />}</Route>
             <Route component={NotFound}></Route>
         </Switch>
     )
