ecc379833883ffc9ca64cb927bf6510fe55a7cc3
marcusdevsurfer <marcus.sea13@gmail.com>
2025-04-04 16:12:46 -0600

    feat: code refactoring and more ui improvement

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react';
-import { fetchAndSetLoans } from './service/LoanService';
+import { fetchLoans } from './service/LoanService';
 import { DashboardCard } from './components/DashboardCard'
 import { LoansTable } from './components/LoansTable';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import { CiDollar, CiUser, CiAlignBottom } from "react-icons/ci";
+import { CiDollar, CiAlignBottom } from "react-icons/ci";
 import './App.css'
+import { Spinner, Stack } from 'react-bootstrap';
 
 function App() {
   const [loansState, setLoansState] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    fetchAndSetLoans(setLoansState)
-    setIsLoading(false)
+    fetchData()
   }, [])
+
+  const fetchData = async () => {
+    const response = await fetchLoans()
+    setLoansState(response)
+    setIsLoading(false)
+  }
 
   const sumValuesByKey = (collection, key) => {
     return collection.map((e) => e[key]).reduce((pv, cv) => pv + cv, 0);
@@ -25,23 +31,36 @@ function App() {
   }
 
   return (
-    <Container>
-      <Row className='mt-3 mb-4'>
-        <h1 className='dashboard-title'>Panel de Administración de Préstamos</h1>
+    <Container fluid className='px-4 px-sm-5 pt-3 pt-sm-4 container-bg'>
+      <Row className='mb-4'>
+        <h1 className='text-center text-md-start dashboard-title m-0 p-0'>Panel de Administración de Préstamos</h1>
       </Row>
       <Row className="justify-content-evenly px-2 py-3 border rounded mb-2">
-        <h2 className='dashboard-title'>Resumen</h2>
+
         {
-          !isLoading &&
-          <>
-            <DashboardCard text={"Total Prestado"} data={sumValuesByKey(filterPendingLoans(loansState), 'amount')} icon={<CiAlignBottom color='#000' size={'20'} />} dollarSign />
-            <DashboardCard text={"Préstamos Activos"} data={filterPendingLoans(loansState)?.length} icon={<CiDollar color='#000' size={'20'} />} />
-          </>
+          !isLoading ?
+            <>
+              <h2 className='dashboard-title'>Resumen</h2>
+              <DashboardCard text={"Total Prestado"} data={sumValuesByKey(filterPendingLoans(loansState), 'amount')} icon={<CiAlignBottom color='#000' size={'20'} />} dollarSign />
+              <DashboardCard text={"Préstamos Activos"} data={filterPendingLoans(loansState)?.length} icon={<CiDollar color='#000' size={'20'} />} />
+            </>
+            :
+            <Stack direction='horizontal' gap={2} className='justify-content-center'>
+              <Spinner animation='grow' size='sm' />
+              <Spinner animation='grow' size='sm' />
+              <Spinner animation='grow' size='sm' />
+
+            </Stack>
+
         }
       </Row>
-      <Row>
-        <LoansTable />
-      </Row>
+      {
+        !isLoading
+        &&
+        <Row className="justify-content-evenly px-2 py-3 border rounded mb-2">
+          <LoansTable data={loansState} fetchData={fetchData} />
+        </Row>
+      }
     </Container>
   )
 }
